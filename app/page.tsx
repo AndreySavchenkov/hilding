1fa07970d12c39c632fa56adc8ae639e72a359e6
AndreySavchenkov <sannndvik16@gmail.com>
2024-07-28 13:48:31 +0200

    feat: start project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="">
-      start app
+    <main className="flex flex-col justify-center gap-5 p-10">
+      <Button asChild>
+        <Link href="/worker">Продолжить как работник</Link>
+      </Button>
+      <Button asChild>
+        <Link href="/driver">Продолжить как вузковый </Link>
+      </Button>
+      <Button asChild>
+        <Link href="/admin">Продолжить как админ</Link>
+      </Button>
     </main>
   );
 }
---
+++ components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export const Header = () => {
+  return (
+    <div className="bg-slate-700 text-white px-10 py-4">
+      <Link href="/">Hilding Anders</Link>
+    </div>
+  );
+};
