381c5dfbcd5db1274faca41817185d3397eccd9b
AndreySavchenkov <sannndvik16@gmail.com>
2024-10-06 19:43:16 +0200

    fix: delete logs in sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,7 @@
 self.addEventListener("push", function (event) {
-  // Проверяем, пришли ли данные в событии
   if (event.data) {
-    // Логируем данные в виде строки для просмотра
-    console.log("Push event data received:", event.data);
+    const data = event.data.json();
 
-    // Парсим данные из push-уведомления
-    const data = event.data.json(); // Парсим данные как JSON
-
-    // Логируем распарсенные данные
-    console.log(`data in sw.js ->> ${JSON.stringify(data)}`);
-
-    // Используем данные для показа уведомления
     self.registration.showNotification(data.title, {
       body: data.message,
     });
