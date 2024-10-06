e6cb707dbd8a4e00fbe62bd383a945990993ffdb
AndreySavchenkov <sannndvik16@gmail.com>
2024-10-06 15:37:07 +0200

    fix: use suspense and orderForm in order page

---
+++ app/worker/order/components/order-form/OrderForm.tsx
@@ -0,0 +1,145 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useToast } from "@/components/ui/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useSearchParams } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const FormSchema = z.object({
+  pallets: z.boolean().default(false).optional(),
+  scotchTape: z.boolean().default(false).optional(),
+  whiteBraid: z.boolean().default(false).optional(),
+});
+
+export default function Order() {
+  const searchParams = useSearchParams();
+
+  const { toast } = useToast();
+
+  const line = searchParams?.get("line");
+  const area = searchParams?.get("area");
+  const workerNumber = searchParams?.get("workerNumber");
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      pallets: false,
+      scotchTape: false,
+      whiteBraid: false,
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof FormSchema>) => {
+    const orderOptions = {
+      areaOptions: area,
+      lineOptions: line,
+      workerNumber: workerNumber,
+      pallets: data.pallets,
+      scotchTape: data.scotchTape,
+      whiteBraid: data.whiteBraid,
+    };
+
+    try {
+      const apiUrl = "/api/order/create-order-options";
+
+      const requestData = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ orderOptions }),
+      };
+
+      const response = await fetch(apiUrl, requestData);
+
+      if (!response.ok) {
+        throw new Error(
+          `Failed to post worker order option: ${response.status} - ${response.statusText}`
+        );
+      }
+
+      toast({
+        title: "Заказ сделан успешно!",
+        description: `${data?.pallets && "Палеты"} ${
+          data?.scotchTape && "Белый стрейч"
+        } ${data?.scotchTape && "Скотч"}`,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="pallets"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Паллеты:</FormLabel>
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="scotchTape"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Скотч:</FormLabel>
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="whiteBraid"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Белый стрейч:</FormLabel>
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit">Send</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
