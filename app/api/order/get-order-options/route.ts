8d6192f6caf57cf99b7234a715c903bc11c76446
AndreySavchenkov <sannndvik16@gmail.com>
2024-10-06 18:55:18 +0200

    fix: disabling caching for get order options

--- app/api/order/get-order-options/route.ts
+++ app/api/order/get-order-options/route.ts
@@ -3,19 +3,25 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    //fetch options from the db
     const options = await db.orderOptions.findMany({
       orderBy: {
         createdAt: "desc",
       },
     });
 
-    // respond with the options
-    return NextResponse.json(options, { status: 200 });
+    const response = NextResponse.json(options, { status: 200 });
+
+    response.headers.append(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, proxy-revalidate"
+    );
+    response.headers.append("Pragma", "no-cache");
+    response.headers.append("Expires", "0");
+
+    return response;
   } catch (error) {
     console.log("[GET ORDER_OPTIONS]", error);
 
-    // Handle errors
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
