5fa18b497fab5db7f6a6e1f7546847addf64957b
AndreySavchenkov <sannndvik16@gmail.com>
2024-10-07 09:00:11 +0200

    feat: add sw.js and icon for notification

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,21 @@
 self.addEventListener("push", function (event) {
   if (event.data) {
     const data = event.data.json();
-
-    self.registration.showNotification(data.title, {
-      body: data.message,
-    });
-  } else {
-    console.log("Push event but no data");
+    const options = {
+      body: data.body,
+      icon: data.icon || "/notification.png",
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: "2",
+      },
+    };
+    event.waitUntil(self.registration.showNotification(data.title, options));
   }
+});
+
+self.addEventListener("notificationclick", function (event) {
+  console.log("Notification click received.");
+  event.notification.close();
+  event.waitUntil(clients.openWindow("<https://hilding.vercel.app/>"));
 });
