8b6f4dba32f80205570ce8922e58aeed67f9c630
AndreySavchenkov <sannndvik16@gmail.com>
2024-12-18 20:35:52 +0100

    feat: add order creation timestamp display in Order

--- app/driver/components/Order/Order.tsx
+++ app/driver/components/Order/Order.tsx
@@ -7,6 +7,8 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
 import { motion } from "framer-motion";
+import { formatDistanceToNow } from "date-fns";
+import { pl } from "date-fns/locale";
 
 type Props = {
   item: OrderType;
@@ -74,36 +76,44 @@ export const Order = ({ item, setOptions }: Props) => {
 
   return (
     <motion.div
-    key={item.id}
-    initial={{ 
-      opacity: 0, 
-      scale: 0.8,
-      y: -100 // начальная позиция выше на 100px
-    }}
-    animate={{ 
-      opacity: 1, 
-      scale: 1,
-      y: 0 // конечная позиция
-    }}
-    transition={{
-      duration: 0.5,
-      ease: [0.4, 0, 0.2, 1],
-    }}
-    exit={{
-      opacity: 0,
-      scale: 0.8,
-      x: 100,
-      y: -100,
-      rotate: 15,
-      transition: {
+      key={item.id}
+      initial={{
+        opacity: 0,
+        scale: 0.8,
+        y: -100, // начальная позиция выше на 100px
+      }}
+      animate={{
+        opacity: 1,
+        scale: 1,
+        y: 0, // конечная позиция
+      }}
+      transition={{
         duration: 0.5,
-        ease: "easeOut"
-      }
-    }}
+        ease: [0.4, 0, 0.2, 1],
+      }}
+      exit={{
+        opacity: 0,
+        scale: 0.8,
+        x: 100,
+        y: -100,
+        rotate: 15,
+        transition: {
+          duration: 0.5,
+          ease: "easeOut",
+        },
+      }}
       className="flex gap-3 flex-col text-gray-300 bg-gradient-to-br from-blue-600/20 to-purple-600/20 p-4 rounded-xl shadow-lg border border-white/10 backdrop-blur-sm"
     >
       <span className="text-xl md:text-2xl font-medium text-center py-3 border-b border-gray-500/30 text-gray-200">
         Linia: {item.lineOptions} ({AreaOptionsEnum[item.areaOptions]})
+      </span>
+
+      <span className="text-sm text-gray-400 text-center">
+        Zamówiono:{" "}
+        {formatDistanceToNow(new Date(item.createdAt), {
+          addSuffix: true,
+          locale: pl,
+        })}
       </span>
 
       <div className="flex gap-6 flex-col mt-2">
