762931d160ea7dfdb3aae8a93bacaa40de96f9b2
AndreySavchenkov <sannndvik16@gmail.com>
2024-12-22 20:39:28 +0100

    refactor: move utility functions from helpers/index.ts to app/admin/page.tsx for better organization and accessibility

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -12,11 +12,33 @@ import {
 } from "@/components/ui/accordion";
 import refreshIcon from "@/public/refresh.png";
 import Image from "next/image";
-import {
-  calculateDeliveryTime,
-  formatName,
-  getDeliveryTimeColor,
-} from "@/helpers";
+import { differenceInMinutes, differenceInHours } from "date-fns";
+
+export const formatName = (firstName: string, lastName: string) => {
+  const formattedFirstName =
+    firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
+  const formattedLastName = lastName.charAt(0).toUpperCase();
+
+  return `${formattedFirstName} ${formattedLastName}.`;
+};
+
+export const calculateDeliveryTime = (createdAt: Date, deliveredAt: Date) => {
+  const minutesDiff = differenceInMinutes(deliveredAt, createdAt);
+  const hoursDiff = differenceInHours(deliveredAt, createdAt);
+
+  if (hoursDiff > 0) {
+    return `${hoursDiff} godz ${minutesDiff % 60} min`;
+  }
+  return `${minutesDiff} min`;
+};
+
+export const getDeliveryTimeColor = (createdAt: Date, deliveredAt: Date) => {
+  const minutesDiff = differenceInMinutes(deliveredAt, createdAt);
+
+  if (minutesDiff <= 20) return "text-green-500";
+  if (minutesDiff <= 60) return "text-yellow-500";
+  return "text-red-500";
+};
 
 interface User {
   id: string;
