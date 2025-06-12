5d2b7ca6ab208391ec6d1460cae8e7150c8e6c6c
AndreySavchenkov <sannndvik16@gmail.com>
2025-06-12 21:47:05 +0200

    refactor: simple style for worker link

--- app/components/WorkerLink.tsx
+++ app/components/WorkerLink.tsx
@@ -14,22 +14,13 @@ export const WorkerLink = ({ icon, title, href }: WorkerLinkProps) => {
   return (
     <Button
       asChild
-      className="group flex w-full max-w-[250px] h-[250px] bg-gradient-to-br from-blue-600/20 to-purple-600/20 hover:from-blue-600/30 hover:to-purple-600/30 border border-white/10 backdrop-blur-sm transition-all duration-500 rounded-xl overflow-hidden shadow-lg hover:shadow-purple-500/25 hover:scale-105 relative"
+      className="flex   w-[250px] h-[250px] bg-gradient-to-br from-blue-600/20 to-purple-600/20 border border-white/10 backdrop-blur-sm rounded-xl overflow-hidden shadow-lg"
     >
       <Link href={href}>
-        <div className="absolute inset-0 bg-gradient-to-br from-blue-600/20 to-purple-600/20 opacity-0 group-hover:opacity-100 transition-opacity duration-500" />
-        <div className="relative flex flex-col gap-6 items-center">
-          <div className="relative">
-            <div className="absolute -inset-1 bg-purple-600 rounded-full blur opacity-0 group-hover:opacity-75 transition-opacity duration-500" />
-            <Image
-              src={icon}
-              width={150}
-              height={150}
-              alt="worker icon"
-              className="relative transform group-hover:scale-110 group-hover:rotate-3 transition-transform duration-500"
-            />
-          </div>
-          <span className="text-xl font-bold bg-gradient-to-r from-blue-300 to-purple-400 bg-clip-text text-transparent group-hover:from-blue-400 group-hover:to-purple-500 transition-all duration-500">
+        <div className="flex flex-col gap-6 items-center">
+          <Image src={icon} width={80} height={80} alt="worker icon" />
+
+          <span className="text-xl font-bold bg-gradient-to-r from-blue-300 to-purple-400 bg-clip-text text-transparent ">
             {title}
           </span>
         </div>
